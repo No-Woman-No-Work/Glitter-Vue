8fcc55fd3b4d744a1c0780626b4704aa5220c0e6
= <=>
2023-02-04 17:32:24 +0100

    Fix flitterApi

--- src/api/flitterApi.js
+++ src/api/flitterApi.js
@@ -8,12 +8,15 @@ Base URL pendiente de añadir
 
 */
 
+// creamos La conexión con la api (backend)
 const flitterApi = axios.create({
       baseURL: "https://api.aquivalaurldelbakendflitter.co/api/loquesea"
 })
 
+// si en cada petición que se haga con los métodos post o get de flitterapi se recibe un token
+// se configuran los encabezados ( sí token es falsy, se queda como cadena vacía)
 flitterApi.interceptors.request.use(config =>  {
-      const token = localStoragee.getItem('accessToken') || ''
+      const token = localStorage.getItem('accessToken') || ''
       if(token) {
             config.headers["Authorization"] = `Bearer ${token}`
       }
