587942c27c2f88077b4414a8bbdb3c088fc52ad9
Sil <pescadorlopezs@gmail.com>
2023-02-05 13:11:58 +0100

    change token name in flitterApi connection

--- src/api/flitterApi.js
+++ src/api/flitterApi.js
@@ -16,7 +16,7 @@ const flitterApi = axios.create({
 // si en cada petición que se haga con los métodos post o get de flitterapi se recibe un token
 // se configuran los encabezados ( sí token es falsy, se queda como cadena vacía)
 flitterApi.interceptors.request.use(config =>  {
-      const token = localStorage.getItem('accessToken') || ''
+      const token = localStorage.getItem('access_token') || ''
       if(token) {
             config.headers["Authorization"] = `Bearer ${token}`
       }
