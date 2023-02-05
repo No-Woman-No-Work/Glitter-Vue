156568e6c261127f7ba85812c9c38794840fa889
Andrea Ares <110069991+aresaurus@users.noreply.github.com>
2023-02-05 18:43:02 +0100

    Add method for the component

--- src/components/CustomCard.vue
+++ src/components/CustomCard.vue
@@ -7,12 +7,14 @@
 			</p>
 			<slot></slot>
 			<div class="d-grid gap-2">
-				<button v-for="btn in btns" :key="btn.txt" class="btn" :class="btn.class">
+				<button v-for="btn in btns" :key="btn.txt" class="btn" :class="btn.class"
+					@click.prevent="btn.action ? resetPassword : null">
 					{{ btn.txt }}
 				</button>
 				<router-link :to="link.route" v-for="link in link" :key="link.txt" class="link" :class="link.class">
 					{{ link.txt }}
 				</router-link>
+				<p class="text-danger" v-if="error">{{ error }}</p>
 			</div>
 		</div>
 	</div>
@@ -21,7 +23,12 @@
 <script>
 export default {
 	name: 'CustomCard',
-	props: ['title', 'parrafo', 'btns', 'link'],
+	props: ['title', 'parrafo', 'btns', 'link', 'error'],
+	methods: {
+		resetPassword() {
+			this.$emit('reset-password');
+		}
+	}
 }
 </script>
 
@@ -31,3 +38,4 @@ export default {
 	box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
 }
 </style>
+
