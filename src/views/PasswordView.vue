287e7da14076c55a5dc0a62147bd55abbf726539
Sil <pescadorlopezs@gmail.com>
2023-02-05 12:36:28 +0100

    refactorize PasswordView.vue to composition api for vue 3 , integrate setup() and reactive variables with ref and v-model

--- src/views/PasswordView.vue
+++ src/views/PasswordView.vue
@@ -1,65 +1,65 @@
 <template>
 	<div class="home">
-		<div class="row">
-			<div class="col-lg-6">
-				<div class="img-cont">
-					<img alt="Flitter logo" src="../assets/img/logo_flitter.svg" />
-					<h2 class="text-center text-lg-start">
-						Welcome to Flitter
-					</h2>
-				</div>
-			</div>
-			<div class="col-lg-6">
-				<custom-card :btns="btnArray" class="mx-auto custom-card">
-            <h4 class="text-center">
-            Forgot password
-            </h4>
-            <div class="change-pass">
-                <p>Change your password in three easy steps:</p>
-                <ol class="list-unstyled">
-                    <li><span class="text-primary text-medium">1. </span>Enter your email address below.</li>
-                    <li><span class="text-primary text-medium">2. </span>Our system will send you a temporary link.</li>
-                    <li><span class="text-primary text-medium">3. </span>Use the link to reset your password.</li>
-                </ol>
-            </div>
-        <form>
-          <div class="mb-3 mt-2">
-            <input
-              type="email"
-              class="form-control"
-              placeholder="✉️ E-mail"/>
-            </div>
-        </form>
-				</custom-card>
-			</div>
+	  <div class="row">
+	    <div class="col-lg-6">
+		<div class="img-cont">
+		  <img alt="Flitter logo" src="../assets/img/logo_flitter.svg" />
+		  <h2 class="text-center text-lg-start">
+		    Welcome to Flitter
+		  </h2>
 		</div>
+	    </div>
+	    <div class="col-lg-6">
+		<custom-card :btns="btnArray" class="mx-auto custom-card">
+		  <h4 class="text-center">
+		    Forgot password
+		  </h4>
+		  <div class="change-pass">
+		    <p>Change your password in three easy steps:</p>
+		    <ol class="list-unstyled">
+			<li><span class="text-primary text-medium">1. </span>Enter your email address below.</li>
+			<li><span class="text-primary text-medium">2. </span>Our system will send you a temporary link.</li>
+			<li><span class="text-primary text-medium">3. </span>Use the link to reset your password.</li>
+		    </ol>
+		  </div>
+		  <form>
+		    <div class="mb-3 mt-2">
+			<input
+			  type="email"
+			  class="form-control"
+			  placeholder="✉️ E-mail"
+			  v-model="email"/>
+		    </div>
+		  </form>
+		</custom-card>
+	    </div>
+	  </div>
 	</div>
-  <FooterSection/>
-</template>
-
-
-<script>
-	// @ is an alias to /src
-	import CustomCard from '../components/CustomCard.vue'
-  import FooterSection from "@/components/FooterSection.vue"
-	export default {
-		name: 'SignupView',
-		components: {
-			CustomCard,
-            FooterSection,
-		},
-		data() {
-			return {
-				btnArray: [
-					{
-						txt: 'Reset Password',
-						class: 'btn-primary',
-					}
-				],
-			}
-		},
+	<FooterSection />
+    </template>
+    
+    <script>
+    import { ref } from 'vue'
+    import CustomCard from '../components/CustomCard.vue'
+    import FooterSection from "@/components/FooterSection.vue"
+    
+    export default {
+	name: 'SignupView',
+	components: {
+	  CustomCard,
+	  FooterSection
+	},
+	setup() {
+	  const email = ref('')
+	  const btnArray = [{ txt: 'Reset Password', class: 'btn-primary' }]
+    
+	  return {
+	    email,
+	    btnArray
+	  }
 	}
-</script>
+    }
+    </script>
 
 <style scoped>
 	.home {
