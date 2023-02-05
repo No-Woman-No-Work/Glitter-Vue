594dedfe6fe6e4474449cd881e3c470820c7f8b6
Belén Jiménez <b.jimenezsan5@gmail.com>
2023-02-05 17:08:14 +0000

    Se añade PublicView con los mensajes de Flitter

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,12 @@ const routes = [
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ '../views/PasswordView.vue')
+  },
+
+  {
+      path: '/publicview',
+      name: '/publicview',
+      component: () => import(/* webpackChunkName: "about" */ '../views/PublicView.vue')
   }
 ]
 
