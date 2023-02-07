8b33fa2ae5f77d3e7cca60ce44491062afbd36fa
aresaurus <andrearesfdez@gmail.com>
2023-02-07 22:56:36 +0100

    Update NavBar.vue component with a search bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,72 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-light">
-       <div class="container justify-content-evenly d-flex align-items-center text-center">
-           <router-link to="/" class="nav-link active" aria-current="page">
-            <span class="material-icons">home</span>
-           </router-link>
-       </div>
-   </nav>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container justify-content-evenly d-flex align-items-center text-center">
+      <router-link to="/" class="nav-link active" aria-current="page">
+        <span class="material-icons">home</span>
+      </router-link>
+      <form @submit.prevent="search">
+        <input type="text" placeholder="Search flits..." v-model="searchTerm" />
+        <button type="submit" onClick={search}>Go</button>
+      </form>
+    </div>
+  </nav>
 </template>
  
 <script>
- export default {
-   name: "NavBar"
- }
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import flitterApi from "../api/flitterApi";
+
+export default {
+  name: 'NavBar',
+  setup() {
+    const searchTerm = ref('');
+    const router = useRouter();
+
+    // The function returns immediately if the value of searchTerm is falsy to prevent sending an empty search to the server.
+    const search = async () => {
+      if (!searchTerm.value) {
+        return;
+      }
+      // TODO: add endpoint
+      try {
+        const response = await flitterApi.get(`/tweets?search=${searchTerm.value}`);
+        const data = response.data;
+
+        if (data.length > 0) {
+          router.push({
+            // TO DO: create these views and routers
+            name: 'search-results',
+            params: {
+              searchTerm: searchTerm.value,
+            },
+          });
+        } else {
+          router.push({
+            name: 'not-found',
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return {
+      searchTerm,
+      search,
+    };
+  },
+};
 </script>
 
 <style scoped>
 .material-icons {
- color: white;
- font-size: 1.4em;
- background-color: #A0C3D2;
- padding: 0.3em;
- border-radius: 50%;
- margin: 0.1em;
+  color: white;
+  font-size: 1.4em;
+  background-color: #A0C3D2;
+  padding: 0.3em;
+  border-radius: 50%;
+  margin: 0.1em;
 }
 </style>
+
