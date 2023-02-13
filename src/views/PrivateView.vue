62e0e572df50a814adf97cc62a8fc6100dde1821
= <=>
2023-02-13 22:21:31 +0100

    Fix paginator

--- src/views/PrivateView.vue
+++ src/views/PrivateView.vue
@@ -136,73 +136,88 @@ letter-spacing: 2px;
 .tweets-container {
 margin-top: 1em;
 }
+
+/* 
+
+Paginator
+
+*/
+
+
 .paginator {
   text-align: center;
   }
-  .paginator .paginate-buttons {
+.paginator .paginate-buttons {
   width: 40px;
   height: 40px;
   cursor: pointer;
   background: #ffa580;;
-	letter-spacing: 2px;
-	transition: .2s all ease-in-out;
-	outline: none;
-	border: 1px solid rgba(0, 0, 0, 1);
-	box-shadow: 3px 3px 1px 1px #95a4ff, 3px 3px 1px 2px rgba(0, 0, 0, 1);
-  }
-  
-  .paginator .back-button,
-  .paginator .next-button {
-    margin-inline: 10px;
-    border-radius: 25px;
-  }
-  
-  .paginator .first-button {
-    border-start-start-radius: 25px;
-    border-end-start-radius: 25px;
-  }
-  .paginator .last-button {
-    border-start-end-radius: 25px;
-    border-end-end-radius: 25px;
-  }
-  
-  .paginator .back-button svg {
-    transform: rotate(180deg) translateY(-2px);
-  }
-  .paginator .next-button svg {
-    transform: translateY(2px);
-  }
-  .paginator li:nth-child(2) > .paginate-buttons.number-buttons {
-    border-start-start-radius: 25px;
-    border-end-start-radius: 25px;
-    transition: none;
-  }
-  .paginator li:nth-last-child(2) > .paginate-buttons.number-buttons {
-    border-start-end-radius: 25px;
-    border-end-end-radius: 25px;
-  }
-  
-  .paginator .active-page {	
-    background-color: #2980b9;	
-    color: #fff;	
-  }	
-  	
-  .paginator .active-page {	
-    background-color: #2980b9;	
-    color: #fff;	
-  }	
-  	
-  .paginator .paginate-buttons:hover {	
-    background-color: #f5f5f5;	
-  }	
-  	
-  .paginator .active-page:hover {	
-    background-color: #388ac1;	
-  }
-  .paginator .back-button:active,
-  .paginator .next-button:active {
-    background-color: #e6e6e6;
-  }
+letter-spacing: 2px;
+transition: .2s all ease-in-out;
+outline: none;
+border: 1px solid rgba(0, 0, 0, 1);
+box-shadow: 3px 3px 1px 1px #95a4ff, 3px 3px 1px 2px rgba(0, 0, 0, 1);
+}
+
+.paginator .back-button,
+.paginator .next-button {
+  margin-inline: 10px;
+  border-radius: 25px;
+}
+
+.paginator .first-button {
+  border-start-start-radius: 25px;
+  border-end-start-radius: 25px;
+}
+.paginator .last-button {
+  border-start-end-radius: 25px;
+  border-end-end-radius: 25px;
+}
+
+.paginator .back-button svg {
+  transform: rotate(180deg) translateY(-2px);
+}
+.paginator .next-button svg {
+  transform: translateY(2px);
+}
+.paginator li:nth-child(2) > .paginate-buttons.number-buttons {
+  border-start-start-radius: 25px;
+  border-end-start-radius: 25px;
+  transition: none;
+}
+.paginator li:nth-last-child(2) > .paginate-buttons.number-buttons {
+  border-start-end-radius: 25px;
+  border-end-end-radius: 25px;
+}
+
+.paginator .active-page {
+  background-color: #99e98e;;
+  color: f8f4e5;
+}
+
+.paginator .active-page {
+  background-color: #99e98e;;
+  color: f8f4e5;
+}
+
+.paginator .paginate-buttons:hover {
+  background-color: #f8f4e5;
+}
+
+.paginator .active-page:hover {
+  background-color: #99e98e;;
+}
+.paginator .back-button:active,
+.paginator .next-button:active {
+  background-color: #e6e6e6;
+}
+
+/* 
+
+Paginator
+
+*/
+
   .search-bar {
     margin-bottom: 1em;
   }
