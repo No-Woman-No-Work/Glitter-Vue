e33060c4a01f5d43f69d9fee2c43aa0d02c718cf
Sil <pescadorlopezs@gmail.com>
2023-02-14 23:04:14 +0100

    clean store , tests views and components

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -19,7 +19,7 @@ export default {
 
 <style>
 .footer {
-  position: fixed;
+  position: -webkit-sticky;
   bottom: 0;
   width: 100%;
   height: 7rem;
--- src/models/tweet.js
+++
@@ -1,7 +0,0 @@
-export const Tweet = {
-      id: Number,
-      text: String,
-      publishDate: String,
-      author: String,
-      kudos: String,
-    };
